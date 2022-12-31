cf8620901328926f6303f99d81b1e251c8aac017
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2022-12-31 23:59:11 +0200

    animation to items

--- React/src/components/ecom-ui/Item/Item.jsx
+++ React/src/components/ecom-ui/Item/Item.jsx
@@ -1,12 +1,32 @@
 import { Button } from "react-bootstrap";
+import { Fragment, useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import { useDispatch } from "react-redux";
+import Spinner from "react-bootstrap/Spinner";
+
 const Item = ({ btnText, actionType, id, title, price, img }) => {
+  const [disabled, setDisabled] = useState(false);
+  const [btnClicked, setBtnClicked] = useState(0);
+
   const { item } = styles;
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (btnClicked === 0) return;
+    setDisabled(true);
+    const debounce = setTimeout(() => {
+      setDisabled(false);
+    }, 400);
+
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [btnClicked]);
+
   const actionHandeler = () => {
     if (actionType === "add") {
       dispatch({ type: "cart/addToCart", payload: id });
+      setBtnClicked((prev) => prev + 1);
     }
   };
 
@@ -21,8 +41,22 @@ const Item = ({ btnText, actionType, id, title, price, img }) => {
       <Button
         variant="info"
         onClick={actionHandeler}
+        disabled={disabled}
       >
-        {btnText || "Add to card"}
+        {disabled ? (
+          <Fragment>
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            Loading...
+          </Fragment>
+        ) : (
+          btnText || "Add to card"
+        )}
       </Button>
     </div>
   );
